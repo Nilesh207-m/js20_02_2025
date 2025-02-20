bf806b7937d15ede61b6e10e95d8547785efd5ff
Nilesh207-m <114664306+Nilesh207-m@users.noreply.github.com>
2025-02-20 15:24:56 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 2027
+let accountEmail = "nilesh@gg.com"
+var accountPassword = "12345"
+// prefer not to use var
+accountCity = "Cuttack"
+let accountState
+console.log(accountId);
+console.table([accountEmail,accountCity,accountPassword,accountState]);
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+// alert("hello") we are using node.js not browser
+//number => 2 to the power 53
+//bigint
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol
+//object
+
+console.log(typeof "Reet")
+console.log(typeof null) //object, important for interview
+console.log(typeof undefined)
